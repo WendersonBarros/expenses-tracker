2526653294e6b3f7e5eafbe48b512eff435d0d8b
WendersonBarros <wendercesar2@gmail.com>
2025-03-22 23:01:38 -0400

    change: adds some new columns to the expense table

--- database/database.sql
+++ database/database.sql
@@ -1,7 +1,10 @@
 CREATE TABLE Expenses (
-  ID int NOT NULL,
-  Type varchar(255) NOT NULL,
-  Name varchar(255) NOT NULL,
-  Date date NOT NULL,
-  CONSTRAINT PK_Expenses PRIMARY KEY(ID)
+  id int NOT NULL,
+  type varchar(255) NOT NULL,
+  name varchar(255) NOT NULL,
+  value numeric(10, 2) NOT NULL, 
+  date date NOT NULL,
+  installments bool NOT NULL DEFAULT FALSE,
+  installments_months varchar[],
+  CONSTRAINT PK_Expenses PRIMARY KEY(id)
 )
